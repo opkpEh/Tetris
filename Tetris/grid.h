a1c74d39e4bc80665a22f7d5377f4742a5bd8014
opkpEh <harshpandeykp20@gmail.com>
2023-09-30 10:22:17 +0530

    base game completed

--- Tetris/grid.h
+++ Tetris/grid.h
@@ -10,9 +10,13 @@ public:
 	int grid[20][10];
 	bool IsCellOutside(int row, int column);
 	bool IsCellEmpty(int row, int column);
+	int ClearFullRows();
 	void Draw();
 
 private:
+	bool IsRowFull(int row);
+	void ClearRow(int row);
+	void MoveRowDown(int row, int numRows);
 	int numRows;
 	int numCols;
 	int cellSize;
